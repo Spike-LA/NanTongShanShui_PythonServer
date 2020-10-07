d3280e9b4a221ab6f3d7e6b22e2d9a26d5d4b668
Spike-LA <kaisss163@163.com>
2020-10-07 18:37:14 +0800

    zhuskai

--- App/serializers/pagination_serializer.py
+++
@@ -1,8 +0,0 @@
-from App.models import MainEngine
-from rest_framework import serializers
-
-
-class PagerSerializer(serializers.ModelSerializer):  # main_engine分页
-    class Meta:
-        model = MainEngine
-        fields = "__all__"
